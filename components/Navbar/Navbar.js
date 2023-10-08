5a0e39c533cb248387e60996582da304d47a0342
AniModi <98632268+AniModi@users.noreply.github.com>
2023-10-09 00:23:22 +0530

    Added Navbar

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+import { FaEthereum } from 'react-icons/fa';
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className={styles.container}>
+        <div className={styles.left}>
+            <FaEthereum className={styles.eth}></FaEthereum>
+            <div className={styles.title}>AbstraZ</div>
+        </div>
+        <div className={styles.middle}>
+            <Link href={"/dashboard"} className={styles.link}>Dashboard</Link>
+            <Link href={"/create"} className={styles.link}>Create DAO</Link>
+            <Link href={"/join"} className={styles.link}>Join DAO</Link>
+        </div>
+        <div className={styles.right}>
+            <div className={styles.dot}>
+                <div className={styles.dot_inner}></div>
+            </div>
+            <div className={styles.chain}>Chain</div>
+        </div>
+      </nav>
+    </>
+  );
+}
