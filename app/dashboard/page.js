7ca85175a3e7d4a8045a7e2e74b9f156ab331381
AniModi <98632268+AniModi@users.noreply.github.com>
2023-10-21 16:05:32 +0530

    Added dropdown

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,11 @@
+import styles from './page.module.css';
+
 export default function Dashboard() {
   return (
     <>
-      <></>
+      <div className={styles.container}>
+        
+      </div>
     </>
   );
 }
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,25 +1,42 @@
+'use client'
+import React, { useState } from 'react';
 import Link from 'next/link';
 import styles from './Navbar.module.css';
 import { FaEthereum } from 'react-icons/fa';
 
 export default function Navbar() {
+  const [selectedChain, setSelectedChain] = useState('Mumbai');
+
+  const handleChainChange = (event) => {
+    setSelectedChain(event.target.value);
+  };
+
   return (
     <>
       <nav className={styles.container}>
         <div className={styles.left}>
-            <FaEthereum className={styles.eth}/>
-            <Link className={styles.title} href={"/"}>AbstraZ</Link>
+          <FaEthereum className={styles.eth} />
+          <Link className={styles.title} href={"/"}>AbstraZ</Link>
         </div>
         <div className={styles.middle}>
-            <Link href={"/dashboard"} className={styles.link}>Dashboard</Link>
-            <Link href={"/create-dao"} className={styles.link}>Create DAO</Link>
-            <Link href={"/join-dao"} className={styles.link}>Join DAO</Link>
+          <Link href={"/dashboard"} className={styles.link}>Dashboard</Link>
+          <Link href={"/create-dao"} className={styles.link}>Create DAO</Link>
+          <Link href={"/join-dao"} className={styles.link}>Join DAO</Link>
         </div>
         <div className={styles.right}>
-            <div className={styles.dot}>
-                <div className={styles.dot_inner}></div>
-            </div>
-            <div className={styles.chain}>Mumbai</div>
+          <div className={styles.dot}>
+            <div className={styles.dot_inner}></div>
+          </div>
+          <div className={styles.chainDropdown}>
+            <select
+              value={selectedChain}
+              onChange={handleChainChange}
+              className={styles.chainSelect} 
+            >
+              <option value="Mumbai">Mumbai</option>
+              <option value="OtherChain">Other Chain</option>
+            </select>
+          </div>
         </div>
       </nav>
     </>
